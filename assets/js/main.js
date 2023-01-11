784323f7f6afa9fcf5c39eb3dc839d9cc796e3ec
Vikashchamoli <105197815+Vikashchamoli@users.noreply.github.com>
2023-01-11 18:27:00 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+
+
+const spans = document.querySelectorAll('.progress-bar span');
+
+spans.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+});
+
+// progress bar
+
+// button
